1501cadd2479684dbb29af784631feac546c8471
cami <kmi.rojas.molina@gmail.com>
2025-12-04 21:08:13 -0500

    added some merge sort annotations

--- sorting/MergeSort.java
+++ sorting/MergeSort.java
@@ -1,4 +1,10 @@
 package sorting;
+/*  
+    Overall time complexity:  ALWAYS O(n(log n)), (best, worst, avr)
+    Space complexity: O(n), because we create subarrays.
+    depending on the og array size.
+*/
+
 public class MergeSort{
     public static void main(String args[]){
         int[] array = {8, 2, 5, 3, 4, 7, 6, 1};
@@ -12,7 +18,8 @@ public class MergeSort{
             System.out.print(array[i] + " " );
         }
     }
-    private static void mergeSort(int[] array) {
+// Splitting only the splitting takes O(log n)
+    private static void mergeSort(int[] array) { 
         int length = array.length;
         if(length <= 1) return; //base case
           
@@ -31,17 +38,18 @@ public class MergeSort{
                 j++;
             }
         }
-        mergeSort(leftArray);
-        mergeSort(rightArray);
-        merge(leftArray, rightArray, array);
+        mergeSort(leftArray); //O(logn)
+        mergeSort(rightArray);  //O(logn)
+        merge(leftArray, rightArray, array); //O(n)
 
     }
-
+// You do this n times, you touch every element once at each log n level. 
+// So this said, complexity takes O(n log n)
     private static void merge(int[] leftArray, int[] rightArray, int[] array){
         int leftSize = array.length / 2;
         int rightSize = array.length - leftSize;
         int i = 0, l = 0, r = 0; //indices
-        //check the ocnditions fro merging
+        //check the ocnditions for merging
         while(l < leftSize && r < rightSize){
             if(leftArray[l] < rightArray[r]){
                 array[i] = leftArray[l];
