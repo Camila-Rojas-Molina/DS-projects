4d274c19466655b113eaf769f24598ea42734202
Camila Rojas <98622277+Camila-Rojas-Molina@users.noreply.github.com>
2026-04-13 16:30:41 -0400

    add DynamicArray class to demonstrate dynamic array operations

---
+++ DataStruturesTest/DynamicArrayDemo.java
@@ -0,0 +1,125 @@
+package DataStruturesTest;
+//import java.util.ArrayList;
+
+public class DynamicArrayDemo {
+    public static class DynamicArray {
+        int size;
+        int capacity = 10;
+        Object[] array;
+
+
+        public DynamicArray() {
+            this.array = new Object[capacity];           
+        }
+
+        public DynamicArray(int capacity) {
+            this.capacity = capacity;
+            this.array = new Object[capacity];
+        }
+
+        public void add(Object data) {
+            if(size >= capacity) {
+                grow();
+            }
+            array[size] = data;
+            size++;
+        }
+
+        public void insert(int index, Object data) {
+            if(size >= capacity){
+                grow();
+            }
+            for(int i =size; i> index; i--){
+                array[i]= array[i-1];
+
+            }
+            array[index] = data;
+            size++;
+        }
+
+        public void delete(Object data) {
+            for(int i= 0; i < size; i++){
+                if(array[i]== data){
+                    for(int j = 0; j<(size-i-1); j++){
+                        array[i+j] = array[i+j+1];
+                    }
+                    array[size-1] = null;
+                    size--;
+                    if(size <=(int) (capacity/3)){
+                        shrink();
+                    }
+                }
+            }
+        }
+
+        public int search(Object data) {
+            return -1;
+        }
+
+        public void grow(){   
+            int newCapacity = (int)(capacity * 2);
+            Object[] newArray = new Object[newCapacity];
+            
+            for(int i = 0; i < size; i++) {
+                newArray[i] = array[i];
+            }
+            capacity = newCapacity;
+            array = newArray;
+        }
+
+        public void shrink() {
+            int newCapacity = (int)(capacity / 2);
+            Object[] newArray = new Object[newCapacity];
+            
+            for(int i = 0; i < size; i++) {
+                newArray[i] = array[i];
+            }
+            capacity = newCapacity;
+            array = newArray;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public String toString() {
+
+            String string = " ";
+
+            for(int i = 0; i < size; i++) {
+                string += array[i] + ", ";
+            }
+            if(string != ""){
+                string = string.substring(0, string.length()-2);
+            }
+            return string;
+        }
+            
+    }
+    public static void main(String[] args) {
+        DynamicArray dynamicArray = new DynamicArray(5);
+        //System.out.println(dynamicArray.capacity);
+        dynamicArray.add("A");
+        dynamicArray.add("B");
+        dynamicArray.add("C");
+        dynamicArray.add("D");
+        dynamicArray.add("E");
+        dynamicArray.add("F");
+
+
+        dynamicArray.insert(1,"holi");
+        dynamicArray.delete("A");
+        dynamicArray.delete("B");
+        dynamicArray.delete("C");
+        dynamicArray.delete("D");
+
+        System.out.println(dynamicArray);
+        System.out.println("size: " + dynamicArray.size);
+        System.out.println("capacity: " + dynamicArray.capacity);
+
+
+
+
+    }
+}
+
