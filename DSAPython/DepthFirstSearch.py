4981aaf5db5b8193ff73a27c16902c931eb28af4
Camila Rojas <98622277+Camila-Rojas-Molina@users.noreply.github.com>
2026-04-16 13:26:49 -0400

    add DepthFirstSearch.py to implement depth first search algorithm

---
+++ DSAPython/DepthFirstSearch.py
@@ -0,0 +1,7 @@
+"""
+Depth first search : It goes as deep as possible 
+along each branch before backtracking/exploring other branches
+
+It's a graph traversal, it uses a stack often implmeented with recursion
+and it's used in algs such as finding cycles 
+"""
